5869b96a09c9d86a79f3cef45d00d7fe40f32286
Lynn Smeria <ae@cephea.de>
2018-08-21 18:07:06 +0200

    Add lightbox-style modal gallery

---
+++ index.js
@@ -0,0 +1,37 @@
+function showGallery(img) {
+  document.body.classList.add('gallery-open');
+  document.querySelector('#gallery .caption').innerHTML = img.getAttribute(
+    'data-gallery-caption'
+  );
+  document
+    .querySelector('#gallery img')
+    .setAttribute('srcset', img.getAttribute('data-gallery-source'));
+  document
+    .getElementById('gallery')
+    .setAttribute('style', 'display: initial').scrollTop = 0;
+}
+
+function hideGallery() {
+  document.body.classList.remove('gallery-open');
+  document.querySelector('#gallery img').setAttribute('srcset', '');
+  document.getElementById('gallery').setAttribute('style', '');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Open gallery when clicking image
+  const images = document.querySelectorAll('img[data-gallery-source]');
+  images.forEach(node =>
+    node.addEventListener('click', () => showGallery(node))
+  );
+
+  // Close gallery when clicking background
+  const gallery = document.getElementById('gallery');
+  gallery.addEventListener('click', () => hideGallery());
+
+  // Close gallery when pressing 'esc'
+  document.onkeydown = event => {
+    if (event.key === 'Escape') {
+      hideGallery();
+    }
+  };
+});
