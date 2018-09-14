7f5edb649fa88fa11e70f2a70031d04e59337944
Lynn Smeria <ae@cephea.de>
2018-09-14 16:43:29 +0200

    Update instalib

--- _scripts/update.sh
+++ _scripts/update.sh
@@ -3,5 +3,7 @@
 yarn
 bundle update
 ./_scripts/writeScheduleData.js
-instalib liberate https://www.instagram.com/camyyssa/ -o _data/instagram.yml
-instalib mirror _data/instagram.yml -f display_url -o assets/instagram/
+npm install -g https://github.com/aengl/instant-liberation
+instalib liberate https://www.instagram.com/camyyssa/ -o _data/instagram.yml -n 12
+rm -f assets/instagram/*.jpg
+instalib mirror _data/instagram.yml -f detail.display_url -o assets/instagram/
