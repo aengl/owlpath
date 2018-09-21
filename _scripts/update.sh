e83724633e030045381e9e3d03fdd24f5527e322
Lynn Smeria <ae@cephea.de>
2018-09-21 10:16:38 +0200

    Document update script

--- _scripts/update.sh
+++ _scripts/update.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash -e
 
+# Update dependencies
 yarn
 bundle update
-./_scripts/writeScheduleData.js
 npm install -g https://github.com/aengl/instant-liberation
+
+# Update schedule
+./_scripts/writeScheduleData.js
+
+# Download Instagram data
 instalib liberate https://www.instagram.com/camyyssa/ -o _data/instagram.yml -n 12
 rm -f assets/instagram/*.jpg
+
+# Mirror Instagram media files
 instalib mirror _data/instagram.yml -f detail.display_url -o assets/instagram/
