4ce9feb42b5cf3a8dce765b58311ef042bdb617f
Lynn Smeria <ae@cephea.de>
2019-04-02 22:15:38 +0200

    Update dependencies & content

--- _scripts/update.sh
+++ _scripts/update.sh
@@ -3,14 +3,13 @@
 # Update dependencies
 yarn
 bundle update
-npm install -g https://github.com/aengl/instant-liberation
 
 # Update schedule
 ./_scripts/writeScheduleData.js
 
 # Download Instagram data
-instalib liberate https://www.instagram.com/camyyssa/ -o _data/instagram.yml -n 12
+npx instalib liberate https://www.instagram.com/camyyssa/ -o _data/instagram.yml -n 12
 rm -f assets/instagram/*.jpg
 
 # Mirror Instagram media files
-instalib mirror _data/instagram.yml -f detail.display_url -o assets/instagram/
+npx instalib mirror _data/instagram.yml -f detail.display_url -o assets/instagram/
