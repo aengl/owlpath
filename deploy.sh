d0e570ede3cb11278395338f727634067602755b
Lynn Smeria <ae@cephea.de>
2019-07-11 20:58:07 +0800

    Improve deployment

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-yarn clean
-
 # The now builder doesn't have access to our gallery images on Resilio, so we
 # copy them into a tmp dir within the build context
 mkdir -p tmp
 cp ${GALLERY_PATH:=~/Resilio\ Sync/Owl\ Path}/* tmp/
 
-# Switch to teamturtle scope and deploy
+# Deploy to production
 npx now --target production
 
 # Clean up
